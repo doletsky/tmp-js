8e2598b18f48cfba6ce3da5bbe4216c94dc50eda
adoletskiy <adoletsky@indigos.ru>
2015-04-24 12:18:53 +0300

    version all obj. - working

--- drag_drop/js/script.js
+++ drag_drop/js/script.js
@@ -4,13 +4,17 @@ $(document).ready(function(){
 
     $('.droppable').droppable({
         drop: function(event, ui) {
+            ui.draggable.css('position','fixed');
             $(this).attr('rating','false');
             ui.draggable.offset({top:$(this).offset().top, left:$(this).offset().left});
             //alert(ui.draggable.children('div').attr('class'));
             if($(this).data('obj')==ui.draggable.children('div').attr('class')){$(this).attr('rating','true');}
             $('.droppable').css('display','none');
         },
-        out:function(){$(this).attr('rating','false');},
+        out:function(event, ui){
+            $(this).attr('rating','false');
+            //ui.draggable.css('position','relative');
+        },
         activeClass: "active",
         hoverClass: "flashing",
         tolerance: "intersect"
