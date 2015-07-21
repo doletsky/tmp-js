20a7e21595b36779096c839f2782ca0bd4dbdde9
adoletskiy <adoletsky@indigos.ru>
2015-07-21 12:49:01 +0300

    debug move

--- test_draginorder_ver.1.0/13/js_13/test_draginorder_jsAction.js
+++ test_draginorder_ver.1.0/13/js_13/test_draginorder_jsAction.js
@@ -1,17 +1,3 @@
-jQuery.fn.swap = function(b) {
-    b = jQuery(b)[0];
-    var a = this[0],
-        a2 = a.cloneNode(true),
-        b2 = b.cloneNode(true),
-        stack = this;
-
-    a.parentNode.replaceChild(b2, a);
-    b.parentNode.replaceChild(a2, b);
-
-    stack[0] = a2;
-    return this.pushStack( stack );
-};
-
 var draginorder={
     /*инициализация функционала теста*/
     screenResol:0,
@@ -46,75 +32,43 @@ var draginorder={
         ob.children("#sortContainer").append(htmlInd);
         ob.children("#sortContainer").children('.sortable').each(function(){
             var did=$(this).attr('id').slice(4);
-            ob.children("#sortContainer").children('#fitem'+did).height($(this).height()); console.log(ob.children("#sortContainer").children('#fitem'+did).position().top);
+            ob.children("#sortContainer").children('#fitem'+did).height($(this).height());
             $(this).css('top',ob.children("#sortContainer").children('#fitem'+did).position().top);
         });
 
-            var meth=this;
+        var meth=this;
         var mXY={};
         var flag={over:0,change:0};
         var drag={};
-        ob.children('#sortContainer').mousemove(function(e){mXY=e;});
-//            ob.children('#sortContainer').addClass('ui-droppable');
+        ob.children('#sortContainer').children('div.droppable').addClass('ui-droppable');
         ob.children('#sortContainer').children('div.sortable').draggable({
                 axis: "y",
-                containment:"#sortContainer",
+                containment:"parent",
                 start: function(event, ui){
-//                    drag.html=$(this).parent().html();
-//                    drag.id=$(this).parent().attr('id');
-//                    drag.fon=$(this).parent().children('.fon').html();
-//                    $(this).parent().height($(this).parent().children('.fon').height());
-//                    $(this).parent().children('.fon').remove();
                     $(this).css('z-index','999');
                 },
                 stop: function(event, ui){
                     $(this).css('z-index','99');
-//                    ob.children('#sortContainer').children('#'+drag.id).append($(this).clone());
-//                    ob.children('#sortContainer').children('#'+drag.id).children('.sortable').draggable("disable");
                 }
             });
-
-            ob.children('#sortContainer').children('div.droppable').droppable({
+        var obCur=ob;
+        console.log(ob.children('#sortContainer').children('div.droppable'));
+        ob.children('#sortContainer').children('div.droppable').droppable({
                 containment: "parent",
                 accept:".sortable",
                 tolerance:"touch",
                 over:function(event, ui)
                 {
-//                    var emptCont=$(ui.draggable[0]).parent('div');
-                    console.log($(this));
-                    console.log($(ui.draggable).attr('id'));
-                    if($(this).children('.sortable').attr('id')!=$(ui.draggable[0]).attr('id'))
+//                    var obCur=$(this).parent('div').parent();
+                    console.log($(this).parent());
+                    if($(this).attr('id')!=$(ui.draggable).attr('data-stay'))
                     {
-                     drag.id=$(this).attr('id');
+                        var moveId=$(this).attr("id");
+                        var moveEl=ob.children('#sortContainer').children('div[data-stay="'+moveId+'"]');
+                        moveEl.attr('data-stay',$(ui.draggable).attr('data-stay'));
+                        $(ui.draggable).attr('data-stay', moveId);
+                        meth.updateTopHeight(obCur);
                     }
-////
-//                        el=$(ui.draggable[0]).after($(this).html());
-//                        el.css('top',0);
-//                        $(ui.draggable[0]).css('position','absolute');
-//                        var hEmpt=$(this).height();
-//                        console.log($(this).height());
-//                        $(this).html($(ui.draggable[0]));
-////                        $(this).height(hEmpt);
-//
-//                        $(ui.draggable[0]).css('top',parseInt($(ui.draggable[0]).css('top'))+40+'px');
-////                        ui.position.top=parseInt($(ui.draggable[0]).css('top'))+40;
-////                        ui.offset.top=ui.offset.top+40;
-//                    }
-////                    console.log($(this).offset().top);
-//                    $(ui.draggable[0]).css('z-index','999');
-//////                    $(this).offset({top:$(ui.draggable).offset().top});
-////                    $(this).offset({top:$(this).offset().top+20});
-////                    console.log($(this).offset().top);
-////                    drag=this;
-//                    flag.f=1;
-////                    el=$(ui.draggable[0]).after($(this).removeClass('border-red'));
-////                    $(ui.draggable[0]).offset({top:$(ui.draggable[0]).offset().top+40});
-////                    el.removeAttr('style');
-////                    el.droppable( "enable" );
-////                    $(this).remove();
-////                    $(ui.draggable[0]).removeAttr('style');
-////                    $(ui.draggable[0]).mouseup();
-
                 },
 //                drop:function(event, ui)
 //                {
@@ -123,19 +77,26 @@ var draginorder={
 //                },
                 hoverClass:"border-red"
 
-            });
+        });
 
         this.indicateTrue(ob);
     },
-    mix: function(ar){
+    mix: function(ar){console.log('-----------------');
         var newAr=[];
+        var tmpAr=[];
+        var max=0;
+        var randNum=0;
         if(ar.length>2){
             var oldAr=ar;
-            for(var i=0;i<ar.length+1;i++){
-                var max=oldAr.length-1;
-                var randNum=Math.floor(Math.random() * (max  + 1));
+            for(var i=0;i<ar.length-1;i++){
+                max=oldAr.length-1;
+                randNum=Math.floor(Math.random() * (max  + 1));
                 newAr[newAr.length]=oldAr[randNum];
-                oldAr.splice(randNum, 1);
+                for(var j=0;j<oldAr.length;j++){
+                   if(j!=randNum) tmpAr[tmpAr.length]=oldAr[j];
+                }
+                oldAr=tmpAr;
+                tmpAr=[];
             }
             newAr[newAr.length]=oldAr[0];
         }else{
@@ -162,86 +123,21 @@ var draginorder={
         var countError=error;
         ob.children('#sortContainer').unbind();$('.sortable').unbind();
         return [trueLine, error, countItem, countItem];/*кол-во верных, кол-во неверных, д.б. верных, id задания*/
+    },
+    updateTopHeight: function(ob){
+        ob.children("#sortContainer").children('.sortable').each(function(){
+            var did=$(this).attr('data-stay');
+            ob.children("#sortContainer").children('#'+did).height($(this).height());
+        });
+        ob.children("#sortContainer").children('.sortable').each(function(){
+
+            if(!$(this).hasClass('ui-draggble-dragging')){
+                var did=$(this).attr('data-stay');console.log(ob.children("#sortContainer").children('#'+did));
+                $(this).css('top',ob.children("#sortContainer").children('#'+did).position().top);
+            }
+
+        });
     }
 }
 
 
-
-//
-//
-///*инициализация теста*/
-//var screenResol=0;
-//var tableHeight=0;
-//var modeFlagCoach=0; /* 0 - тренировочный , 1 - контролирующий */
-//var numItem=0;
-//var workArea= new Object();
-//
-//function initTest(){
-//
-//    var arKeyJobInd=mix(Object.keys(test.jobData)); /* mix - вызов функции, перемешивающей правильную послед-ть */
-//    $(".job_exercises").html('');
-//    $(".job_exercises").append("<div id='sortContainer'></div>");
-//    var htmlInd='';
-//    for(var ind=0; ind<arKeyJobInd.length; ind++){
-//        htmlInd+='<div id="item'+arKeyJobInd[ind]+'" class="sortable moving">'+test.jobData[arKeyJobInd[ind]].dataText+'</div>';
-//    }
-//    $("#sortContainer").append(htmlInd);
-//    $(function() {
-//
-//        $('#sortContainer').sortable({
-//            update: function(event, ui) {
-//                indicateTrue();
-//                if(noFirstAnswer==0){
-//                    noFirstAnswer=1;
-//                    $(".check_your").css({"background": "url('../styles/img/5.png') no-repeat", "background-size":"auto 100%"});
-//                }
-//            },
-//            containment: "parent",
-//            tolerance: "pointer"
-//        });
-//
-//    });
-//    indicateTrue();
-//}
-//
-//function mix(ar){
-//    var newAr=[];
-//    if(ar.length>2){
-//        var oldAr=ar;
-//        for(var i=0;i<ar.length+1;i++){console.log(i);
-//            var max=oldAr.length-1;console.log('max='+max);
-//            var randNum=Math.floor(Math.random() * (max  + 1));console.log('rand='+randNum);
-//            newAr[newAr.length]=oldAr[randNum];
-//            oldAr.splice(randNum, 1);console.log(oldAr);
-//        }
-//        newAr[newAr.length]=oldAr[0];
-//    }else{
-//        newAr=ar;
-//    }
-//    console.log(newAr);
-//    return newAr;
-//}
-//
-//function indicateTrue(){
-//    $('.sortable').each(function(){
-//        //var $(this).attr('id').slice(4,1);
-//        if((1+$('.sortable').index($(this)))==$(this).attr('id').slice(4)){
-//            $(this).attr('data-right','true');
-//        }else{
-//            $(this).attr('data-right','false');
-//        }
-//    });
-//}
-//
-//function controlJob(){
-//    if($('#sortContainer').attr('data-answer')==1)return false;//lock if answering yet
-//    $('#sortContainer').attr('data-answer','1');
-//    var trueLine=$('.sortable[data-right="true"]').length;
-//    var error=$('.sortable[data-right="false"]').length;
-//    var countItem=$('.sortable').length;
-//    var countError=error;
-//    $('#sortContainer').unbind();$('.sortable').unbind();
-//    return [trueLine, error, countItem, countItem];/*кол-во верных, кол-во неверных, д.б. верных, id задания*/
-//}
-//
-//
